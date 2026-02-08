d4a3c01feff517a7658896be6e0aa6edf0093d93
JohnGallemore <122250845+JohnGallemore@users.noreply.github.com>
2026-02-08 06:32:16 -0600

    Finished Basics of Notes
    Finished up the logic that displays the notes for the users on the Notes page. Still need to implement some way of deleting the notes.

--- 2026/frontend/my_app/lib/screens/notes.dart
+++ 2026/frontend/my_app/lib/screens/notes.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class NotesPage extends StatefulWidget {
+final supabase = Supabase.instance.client;
+
+// We use the HookWidget over the StatefulWidget because they handle async data better and are more concise.
+// They also have a lot of built in hooks that make it easier to manage state and side effects.
+class NotesPage extends HookWidget {
   const NotesPage({super.key});
-
-  @override
-  State<NotesPage> createState() => _NotesPageState();
-}
-
-class _NotesPageState extends State<NotesPage> {
+  
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    final future = useMemoized(() => supabase.from('notes').select('title, body, created_at'));
+    final snapshot = useFuture(future);
+
+    if (snapshot.hasError) {
+      return Center(child: Text('Error: ${snapshot.error}'));
+    }
+
+    final List<dynamic> notes = snapshot.data as List<dynamic>? ?? [];
+
+    debugPrint('Notes: $notes');
+
+    return Scaffold(
       appBar: AppBar(title: const Text('Notes')),
-      body: const Center(child: Text('This is the Notes page')),
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index) {
+          final note = notes[index];
+          return ListTile(
+            title: Text(
+              note['title'] ?? 'No title', 
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold)
+            ),
+
+            subtitle: Text(
+              'Created on: ${note['created_at'].toString().substring(0, note['created_at'].toString().indexOf('T'))}\n${note['body'] ?? 'No content'}',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12.0)
+            ),
+          );
+        },
+      ),
     );
   }
 }
