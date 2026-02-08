128e1bb44f68d8d870f83d37574b5bb63c442b97
Mahin Moon <124702885+Xirro9@users.noreply.github.com>
2026-02-08 10:57:16 -0600

    chanes to main

--- 2026/frontend/my_app/lib/main.dart
+++ 2026/frontend/my_app/lib/main.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:my_app/screens/theme/apptheme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    debugPrint("Error loading .env file: $e");
+  }
+
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY']; // or SUPABASE_ANON_KEY
+
+  if (supabaseUrl == null || supabaseUrl.isEmpty) {
+    debugPrint("Missing SUPABASE_URL. Check .env and pubspec.yaml assets.");
+    return;
+  }
+
+  if (supabaseAnonKey == null || supabaseAnonKey.isEmpty) {
+    debugPrint("Missing SUPABASE_API_KEY. Check .env and pubspec.yaml assets.");
+    return;
+  }
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,4 +47,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
